fb81ad3f2ead4272bfd7f7ddadb6036878aa4769
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2013-09-24 17:35:47 +0100

    Add comment to raw_pin example

--- examples/raw_pin.rb
+++ examples/raw_pin.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/frankenpins'
 
+# Create a new Pin to control Wiring Pi pin #0
+# We enable the built-in pull up/down resistors
+# on the pin using the :pull => :up option, the
+# default is :pull => :none
 raw_pin = Frankenpins::Pin.new(:pin => 0, :pull => :up)
 raw_pin.watch do |pin|
   puts "Value changed #{pin.value}"
