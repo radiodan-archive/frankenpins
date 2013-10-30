853eab6d15d09652aeedf16bcefe3339aa771e46
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2013-10-30 11:47:39 +0000

    Specify LED brightness transitions as percentage

--- examples/led.rb
+++ examples/led.rb
@@ -19,19 +19,19 @@ sleep(2)
 # Variable brightness using PWM
 
 puts "Turning to the LED to 20% brightness"
-led.on!(:brightness => 0.2)
+led.on!(:brightness => 20)
 sleep(2)
 
 puts "Turning to the LED to 40% brightness"
-led.on!(:brightness => 0.4)
+led.on!(:brightness => 40)
 sleep(2)
 
 puts "Turning to the LED to 60% brightness"
-led.on!(:brightness => 0.6)
+led.on!(:brightness => 60)
 sleep(2)
 
 puts "Turning to the LED to 80% brightness"
-led.on!(:brightness => 0.8)
+led.on!(:brightness => 80)
 
 sleep(2)
 
