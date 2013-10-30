a3eea2a409e28534b8afeee23324b6988dee57e7
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2013-10-30 11:47:39 +0000

    Specify LED brightness transitions as percentage

--- lib/frankenpins/led.rb
+++ lib/frankenpins/led.rb
@@ -10,27 +10,27 @@ module Frankenpins
       options[:direction] = :out
       @pin = Frankenpins::Pin.new(options)
       @using_pwm = false
+      @pwm_range = 100
+      @pwm_increment_in_secs = 0.01
     end
 
     def use_pwm!
-      @pin.io.soft_pwm_create(@pin.wiring_pin, 0, 100) unless @using_pwm
+      @pin.io.soft_pwm_create(@pin.wiring_pin, 0, @pwm_range) unless @using_pwm
       @using_pwm = true
     end
 
     def brightness=(value)
-      sanitised_brightness = (value * 100).to_i
       use_pwm!
-      @pin.io.soft_pwm_write(@pin.wiring_pin, sanitised_brightness)
+      @pin.io.soft_pwm_write(@pin.wiring_pin, value.to_i)
     end
 
     def on!(opts={:brightness => nil, :duration => nil})
-      # brightness should be a value from 0 - 1
       if opts[:duration]
         fade_for_duration(opts[:duration])
       elsif opts[:brightness]
         self.brightness = opts[:brightness]
       elsif @using_pwm
-        self.brightness = 1
+        self.brightness = @pwm_range
       else
         @pin.write(true)
       end
@@ -48,20 +48,20 @@ module Frankenpins
 
     private
     def fade_for_duration(duration_in_secs, direction=:up)
-      fade_for_duration_in_increments_of(duration_in_secs, 0.01, direction)
+      fade_for_duration_in_increments_of(duration_in_secs, @pwm_increment_in_secs, direction)
     end
 
     def fade_for_duration_in_increments_of(duration_in_secs, increment_time_in_sec, direction)
       increment = increment_time_in_sec.to_f / duration_in_secs.to_f
       steps = (duration_in_secs.to_f / increment_time_in_sec.to_f).to_i
 
-      brightness_value = direction == :up ? 0 : 1
+      brightness_value = direction == :up ? 0 : @pwm_range
 
       steps.times.each do
         if direction == :up
-          brightness_value = brightness_value + increment
+          brightness_value = brightness_value + (increment * @pwm_range)
         else
-          brightness_value = brightness_value - increment
+          brightness_value = brightness_value - (increment * @pwm_range)
         end
 
         self.brightness = brightness_value
